ce7bb653afb56a8ebd0aae38d864a213e89e136c
huajiao925 <lyrali142857@outlook.com>
2021-01-03 18:53:54 +0800

    email schedular

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,17 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const Post = require('../models/post');
+let nodemailer = require('nodemailer');
+let cron = require('node-cron');
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'lyrali917@gmail.com',
+      pass: '<FROM_EMAIL_PASSWORD>'
+    }
+});
+
 
 //get all posts
 router.route('/allpost').get((req, res)=>{
     return res.json;
 })
-
-
+//make a post
 router.route('/makeapost').post(async(req, res)=>{
     var post = new Post(req.body);
     post.save().then(item=> {
         res.status(200).send("Post succeed.")
+        //email schedular
+        let mailOptions = {
+            from: req.body['sendFrom'],
+            to: req.body['receivedBy'],
+            subject: 'Here is your daily post updates',
+            text: 'Update: ' + req.body['post'],,
+        };
+
+        cron.schedule('* * * * *', () => {
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('Email sent: ' + info.response);
+                }
+            });
+        });
     })
     .catch(error => {
         res.status(400).send("Failed.")
